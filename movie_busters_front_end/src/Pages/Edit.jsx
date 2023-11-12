0f189a46c5585f39eba63720f0be71a8a045a791
Genesis Lara <genesislara@pursuit.org>
2023-11-12 14:01:17 -0500

    edit page

---
+++ movie_busters_front_end/src/Pages/Edit.jsx
@@ -0,0 +1,12 @@
+import MovieEditForm from "../Components/MovieEdit";
+
+function Edit() {
+  return (
+    <div className="New Edit">
+      <h2>Edit</h2>
+      <MovieEditForm />
+    </div>
+  );
+}
+
+export default Edit;
