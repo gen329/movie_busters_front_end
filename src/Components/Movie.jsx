1c4e2d6af62ffae0a7adce2ca891b09e0dc163d5
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-11-14 00:15:52 -0500

    revamp logic for fectching single and multiple movies, working

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom";
+// import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Movie({ movie, index}) {
+function Movie({ movie }) {
   return (
-    <tr>
-      <td>
-        {movie.title}
-      </td>
-      <td>
-        <Link to={`/movies/${index}`}>MOVIES</Link>
-      </td>
-    </tr>
+    <div className="movie-item">
+      <h2>{movie.title}</h2>
+      <p>{movie.description}</p>
+      <p>Year of Release: {movie.year_of_release}</p>
+      <p>Genres: {movie.genres.join(', ')}</p>
+      <p>Rating: {movie.rating}</p>
+      <p>Runtime: {movie.runtime} minutes</p>
+      <Link to={`/movies/${movie.id}`}>Details</Link>
+    </div>
   );
 }
 
 export default Movie;
+
+
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,37 +1,38 @@
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
-const API = import.meta.env.VITE_BASE_URL
+const API = import.meta.env.VITE_API_URL;
 
 function Movies() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch(`${API}/movies`)
-    .then((response) => response.json())
-    .then( movies => setMovies(movies))
-    .catch(error => console.log(error))
-  }, [])
+      .then((response) => response.json())
+      .then((data) => setMovies(data.data.payload))
+      .catch((error) => console.error(error));
+  }, []);
 
-  return(
+  return (
     <div className="Movies">
       <section>
         <table>
-          <thread>
+          <thead>
             <tr>
-              <th></th>
-              <th>Take me there</th>
-              <th>See this movie</th>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Year of Release</th>
+              {/* Add more headers as needed */}
             </tr>
-          </thread>
+          </thead>
           <tbody>
-            {movies.map((movie, index) => {
-              return <Movie key={index} movie={movie} index={index} />;
-            })}
+            {movies.map((movie) => (
+              <Movie key={movie.id} movie={movie} />
+            ))}
           </tbody>
         </table>
       </section>
     </div>
-  )
+  );
 }
 
 export default Movies;
