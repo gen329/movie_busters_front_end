21c386ae5ce75098817e7a82e54795b2dbf37812
Genesis Lara <genesislara@pursuit.org>
2023-11-15 02:40:37 -0500

    deletes console.logs

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,48 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-
-// const Movie = ({ movie }) => {
-//   const [posterUrl, setPosterUrl] = useState('');
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchPoster = async () => {
-//       try {
-//         const apiKey = import.meta.env.REACT_APP_OMDB_API_KEY;
-//         const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movie.title)}&apikey=${apiKey}`;
-
-//         const response = await fetch(apiUrl);
-//         if (!response.ok) {
-//           throw new Error('Failed to fetch data');
-//         }
-
-//         const data = await response.json();
-//         if (data.Poster && data.Poster !== 'N/A') {
-//           setPosterUrl(data.Poster);
-//         } else {
-//           setPosterUrl(''); 
-//         }
-//       } catch (error) {
-//         setError(error.message);
-//       }
-//     };
-
-//     fetchPoster();
-//   }, [movie.title]);
-
-//   return (
-//     <tr>
-//       <td>{movie.title}</td>
-//       <td>
-//         {error && <p>Error: {error}</p>}
-//         {posterUrl && <img src={posterUrl} alt="Movie Poster" />}
-//       </td>
-//     </tr>
-//   );
-// };
-
-// export default Movie;
-
-
 import { useState, useEffect } from 'react';
 
 const Movie = ({ movie }) => {
@@ -54,13 +9,8 @@ const Movie = ({ movie }) => {
     const fetchPoster = async () => {
       try {
         const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movie.title)}&apikey=${apiKey}`;
-
         const response = await fetch(apiUrl);
-
-        // Log the response status and data for debugging
-        console.log('Response status:', response.status);
         const responseData = await response.json();
-        console.log('Response data:', responseData);
 
         if (!response.ok) {
           throw new Error('Failed to fetch data');
@@ -72,7 +22,6 @@ const Movie = ({ movie }) => {
           setPosterUrl('');
         }
       } catch (error) {
-        // Log the error for debugging
         console.error('Fetch error:', error);
         setError('Failed to fetch data. Please try again.');
       }
