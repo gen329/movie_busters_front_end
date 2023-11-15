4d49b825a621a9c293e47c0c276d730e2c78bd21
Genesis Lara <genesislara@pursuit.org>
2023-11-15 02:39:28 -0500

    fixes mistake, posters properly added to movies

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,45 +1,80 @@
-// import { Link } from 'react-router-dom';
-// import MoviePoster from './MoviePoster';
+// import React, { useState, useEffect } from 'react';
 
-// function Movie({ movie }) {
+// const Movie = ({ movie }) => {
+//   const [posterUrl, setPosterUrl] = useState('');
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchPoster = async () => {
+//       try {
+//         const apiKey = import.meta.env.REACT_APP_OMDB_API_KEY;
+//         const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movie.title)}&apikey=${apiKey}`;
+
+//         const response = await fetch(apiUrl);
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch data');
+//         }
+
+//         const data = await response.json();
+//         if (data.Poster && data.Poster !== 'N/A') {
+//           setPosterUrl(data.Poster);
+//         } else {
+//           setPosterUrl(''); 
+//         }
+//       } catch (error) {
+//         setError(error.message);
+//       }
+//     };
+
+//     fetchPoster();
+//   }, [movie.title]);
+
 //   return (
-//     <div className="movie-item">
-//       <MoviePoster/>
-//       <h2>{movie.title}</h2>
-//       <p>Year of Release: {movie.year_of_release}</p>
-//       <button className="detail-button">
-//       <Link to={`/movies/${movie.id}`}>Details</Link>
-//       </button>
-//     </div>
+//     <tr>
+//       <td>{movie.title}</td>
+//       <td>
+//         {error && <p>Error: {error}</p>}
+//         {posterUrl && <img src={posterUrl} alt="Movie Poster" />}
+//       </td>
+//     </tr>
 //   );
-// }
+// };
+
+// export default Movie;
 
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const Movie = ({ movie }) => {
   const [posterUrl, setPosterUrl] = useState('');
   const [error, setError] = useState(null);
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 
   useEffect(() => {
     const fetchPoster = async () => {
       try {
-        const apiKey = 'cb21b3b1'; 
         const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movie.title)}&apikey=${apiKey}`;
 
         const response = await fetch(apiUrl);
+
+        // Log the response status and data for debugging
+        console.log('Response status:', response.status);
+        const responseData = await response.json();
+        console.log('Response data:', responseData);
+
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
 
-        const data = await response.json();
-        if (data.Poster && data.Poster !== 'N/A') {
-          setPosterUrl(data.Poster);
+        if (responseData.Poster && responseData.Poster !== 'N/A') {
+          setPosterUrl(responseData.Poster);
         } else {
-          setPosterUrl(''); 
+          setPosterUrl('');
         }
       } catch (error) {
-        setError(error.message);
+        // Log the error for debugging
+        console.error('Fetch error:', error);
+        setError('Failed to fetch data. Please try again.');
       }
     };
 
