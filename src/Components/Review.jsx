1d952b6d206ac136826a76555cd965073f25ae21
Genesis Lara <genesislara@pursuit.org>
2023-11-15 15:23:40 -0500

    fixes syntax errors in Review

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 import ReviewForm from "./ReviewForm";
 
-function Review({ review, handleDelete, handleEdit}) {
+function Review({ review, handleDelete, handleEdit }) {
   const [viewReviewForm, setReviewForm] = useState(false);
+  
   const toggleView = () => {
-    setEditForm(!viewEditForm);
+    setReviewForm(!viewReviewForm);
   };
 
   return (
     <div className="Review">
-      {viewEditForm} ? (
+      {viewReviewForm ? (
         <>
-        <ReviewForm
-          reviewDetails={review}
-          togglleView={toggleView}
-          handleSubmit={handleEdit}
-        />
-        <button onClick={toggleView}>
-          {viewEditForm ? "Cancel" : "Edit this review"}
-        </button>
+          <ReviewForm
+            reviewDetails={review}
+            toggleView={toggleView}
+            handleSubmit={handleEdit}
+          />
+          <button onClick={toggleView}>
+            {viewReviewForm ? "Cancel" : "Edit this review"}
+          </button>
         </>
       ) : (
         <>
-        <h4>
-          {review.title}
-          <span>{review.rating}</span>
-        </h4>
-        <h5>{review}</h5>
-        <button onClick={toggleView}>
-          {viewEditForm ? "Cancel" : "Edit this review"} 
-        </button>
-        <button onClick={() => handleDelete(review.id)}>Delete</button>
+          <h4>
+            {review.title}
+            <span>{review.rating}</span>
+          </h4>
+          <h5>{review.comment}</h5>
+          <button onClick={toggleView}>
+            {viewReviewForm ? "Cancel" : "Edit this review"}
+          </button>
+          <button onClick={() => handleDelete(review.id)}>Delete</button>
         </>
       )}
     </div>
