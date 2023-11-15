3e02e25f965dbce462da68af20c0381adc3d1118
Genesis Lara <genesislara@pursuit.org>
2023-11-14 21:55:52 -0500

    styles buttons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,9 +6,9 @@ export default function NavBar() {
      <header className="banner">
       MOVIE BUSTERS
      </header> 
-      <h2>
-        <Link to="/movies">SEE ALL MOVIES</Link>
-      </h2>
+     <button>
+        <Link to="/movies">Home</Link>
+     </button>
       <button>
         <Link to="/movies/new">New Movie</Link>
       </button>
